9e0c70d61c23edcbb37961fe65f182f4d72d6a0c
saenal <saenal.cha@yanolja.com>
2019-04-17 19:32:07 +0900

    restart project

--- to_do_list_app/server.js
+++ to_do_list_app/server.js
@@ -1,55 +1,26 @@
-/* jslint node: ture */
-'user strict'
+var express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    mongoose = require('mongoose');
 
-var express = require('express')
+mongoose.connect("mongodb://localhost:27017/vuenodedb").then(
+    () => {
+        console.log('Database connection is successful')
+    },
+    err => {
+        console.log('Error when connecting to the database' + err)
+    }
+);
 
-var morgan = require('morgan')
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(cors());
 
-var path = require('path')
+var port = process.env.PORT || 4000;
 
-var app = express()
-
-var mongoose = require('mongoose')
-
-var bodyParser = require('body-parser')
-
-var config = require('./app/Config')
-
-mongoose.connect(config.DB, {
-    useNewUrlParser: true
-})
-
-app.use(express.static(path.join(__dirname, '/public')))
-
-app.use(morgan('dev'))
-
-app.use(bodyParser.json())
-
-app.use(bodyParser.urlencoded({ extended: true }))
-
-var port = config.APP_PORT || 4000
-
-console.log('App listening on port ' + port)
-
-var todoRoutes = require('./app/Routes')
-
-app.use('/api', todoRoutes)
-
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:' + port)
-
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-
-    next()
-})
-
-app.get('/', function (req, res, next) {
-    res.sendFile('./public/index.html')
-})
-
-
-
-
+app.listen( () => {
+    console.log('Listening on port ' + port);
+});
 
