76c9e315ffc6882ac4f03bbad6e7de3bfd45f1ba
saenal <saenal.cha@yanolja.com>
2019-04-17 11:25:53 +0900

    install body parser & create index.ejs

--- to_do_list_app/app.js
+++ to_do_list_app/app.js
@@ -1,41 +1,18 @@
-var createError = require('http-errors');
+//require the just installed express app
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+//then we call express
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+//takes us to the root(/) URL
+app.get('/', function (req, res) {
+  res.render('index');
+});
+//the server is listening on port 3000 for connections
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.set('view engine', 'ejs');
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.post('/addtask', function (req, res) {
+  res.render('index')
 });
-
-module.exports = app;
